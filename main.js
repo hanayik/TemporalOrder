5a5ae6f7bff4489bc9a306fe08886ede8658da50
Taylor Hanayik <hanayik@gmail.com>
2018-02-20 11:00:49 -0500

    added css file

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
-
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
@@ -22,7 +21,7 @@ function createWindow () {
   }))
 
   // Open the DevTools (optional, useful for debugging).
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
