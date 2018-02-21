2a6551bbb390f472d5f9e82aa463f1ab1469d6ee
Taylor Hanayik <hanayik@gmail.com>
2018-02-21 09:59:30 -0500

    added a centered form for session info

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+subjID = getSubjID()
+console.log(subjID)
+
+function getSubjID () {
+  subjID = document.getElementById("subjIDinput").value
+  return subjID
+}
