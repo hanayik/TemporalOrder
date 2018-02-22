13973331d66b81ba8f89e0f43f74e350b6e72408
Kyle Potak <kjpotak@gmail.com>
2018-02-22 13:50:30 -0500

    revision

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-subjID = getSubjID()
-console.log(subjID)
-sessionID = getSessionID()
-console.log(sessionID)
-taskType = getTaskType()
-console.log(taskType)
-button = getButton()
-console.log(button)
+subjID = getSubjID();
+console.log(subjID);
+sessionID = getSessionID();
+console.log(sessionID);
+taskType = getTaskType();
+console.log(taskType);
+button = getButton();
+console.log(button);
+form-area-div = hideForm();
+console.log(form-area-div);
+
 
 
 function getSubjID () {
