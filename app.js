5d9272df3e5fe9cbcc3df15a28626b6ccd6e416a
Kyle Potak <kjpotak@gmail.com>
2018-02-26 22:52:33 -0500

    TaskID text

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 contBtn = document.getElementById('continueButton')
 contBtn.onclick = function () {
   hideForm()
+  getTaskTypeID()
 }
 
 
@@ -21,6 +22,19 @@ function getSessionID() {
 function getTaskType() {
   taskType = document.getElementById("taskType").value;
   return taskType;
+}
+
+function getTaskTypeID() {
+  taskID = document.getElementById)("taskType").value;
+  return taskID;
+
+  var taskID = document.getElementById('taskType').text;
+  document.getElementById("taskType").innerHTML = taskID;
+
+  if (value == 'to-bilateral') {
+    return taskID.options[taskID.selectedIndex].text;
+  }
+  var text = getTaskTypeID('welcome to temp order bilateral')
 }
 //function getButton() {
 //  button = document.getElementById("form-area").innerHtml = "Continue";
